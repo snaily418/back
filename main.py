2e77020ea2bdf359dae0123ed6ddf68c6b997312
kolya <aktoaktov@yandex.ru>
2024-09-15 21:16:08 +0300

    global fix

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+import uvicorn
+
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from auth import auth
-from database import engine
+from database import engine, session
 from models import *
 from routers import api
-from utils import every_day_check
+from services.every_service import every_day_check
 
 Base.metadata.create_all(bind=engine)
 
@@ -30,6 +32,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get('/')
 async def ping():
     return 'pong'
@@ -37,7 +40,13 @@ async def ping():
 
 @app.on_event("startup")
 def startup_event():
+    db = session()
+    
     scheduler = BackgroundScheduler()
     trigger = CronTrigger(hour=1, minute=0)
-    scheduler.add_job(every_day_check, trigger)
+    scheduler.add_job(every_day_check, trigger, args=(db,))
     scheduler.start()
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 import bcrypt
-from services.user_service import get_users, hot_days_update
-from services.task_service import get_tasks
+
 
 def verify_password(plain, hashed):
     password_byte_enc = plain.encode('utf-8')
@@ -12,15 +11,3 @@ def get_password_hash(password):
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password=pwd_bytes, salt=salt)
     return hashed_password
-
-
-def every_day_check(db):
-    users = get_users(db)
-    for user in users:
-        for category_id in range(2):
-            tasks = get_tasks(db, user, category_id)
-            if all([task.checked for task in tasks if task.priority][:2:]):
-                hot_days_update(db, user, True)
-            else:
-                hot_days_update(db, user, False)
-
