986661bab753aa19880214b2ce1f55235d42b26c
kolya <aktoaktov@yandex.ru>
2024-09-15 23:09:11 +0300

    fix

--- services/every_service.py
+++ services/every_service.py
@@ -1,12 +1,14 @@
 from services.user_service import get_users, hot_days_update
 from services.task_service import get_tasks
 
+
 def every_day_check(db):
     users = get_users(db)
     for user in users:
-        for category_id in range(2):
-            tasks = get_tasks(db, user, category_id)
-            if all([task.checked for task in tasks if task.priority][:2:]):
-                hot_days_update(db, user, True)
-            else:
-                hot_days_update(db, user, False)
+        for category in user.categories:
+            if category.permanent:
+                tasks = get_tasks(db, user, category.id)
+                if all([task.checked for task in tasks if task.priority][:2:]):
+                    hot_days_update(db, user, True)
+                else:
+                    hot_days_update(db, user, False)
